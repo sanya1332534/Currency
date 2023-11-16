864d67760edb194b5844585ba99d408cc34a374c
Sarin Oleksandr <sarinoleks@gmail.com>
2023-11-16 02:24:45 +0200

    Hw5

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+from currency.models import Rate, ContactUs
+
+
+def rate_list(request):
+
+    results = []
+    rates = Rate.objects.all()
+
+    for rate in rates:
+        results.append(
+            f'ID: {rate.id}, buy: {rate.buy}, sell: {rate.sell}, type: {rate.currency_type}, '
+            f'source: {rate.source}, created: {rate.created} <br>'
+        )
+
+    return HttpResponse(str(results))
+
+
+def contact_us_list(request):
+
+    results = []
+    feedback = ContactUs.objects.all()
+
+    for contact_us in feedback:
+        results.append(
+            f'ID: {contact_us.id}, email: {contact_us.email_from}, subject: {contact_us.subject}, '
+            f'message: {contact_us.message} <br>'
+        )
+
+    return HttpResponse(str(results))
